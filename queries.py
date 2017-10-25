738304b2819e8e3832c1b2f30628cad1c6898cf6
Bruno Krebs <bruno.krebs@auth0.com>
2017-10-25 15:20:43 -0200

    queries script final

--- queries.py
+++ queries.py
@@ -1,18 +1,53 @@
 # coding=utf-8
 
 # 1 - imports
-from base import Session
-from movie import Movie
+from datetime import date
+
 from actor import Actor
+from base import Session
 from contact_details import ContactDetails
-from stuntman import Stuntman
+from movie import Movie
 
 # 2 - extract a session
 session = Session()
 
-# 3 - extract movies
+# 3 - extract all movies
 movies = session.query(Movie).all()
 
-# 4 - log movies' details
+# 4 - print movies' details
+print('\n### All movies:')
 for movie in movies:
     print(f'{movie.title} was released on {movie.release_date}')
+print('')
+
+# 5 - get movies after 15-01-01
+movies = session.query(Movie) \
+    .filter(Movie.release_date > date(2015, 1, 1)) \
+    .all()
+
+print('### Recent movies:')
+for movie in movies:
+    print(f'{movie.title} was released after 2015')
+print('')
+
+# 6 - movies that Dwayne Johnson participated
+the_rock_movies = session.query(Movie) \
+    .join(Actor, Movie.actors) \
+    .filter(Actor.name == 'Dwayne Johnson') \
+    .all()
+
+print('### Dwayne Johnson movies:')
+for movie in the_rock_movies:
+    print(f'The Rock starred in {movie.title}')
+print('')
+
+# 7 - get actors that have house in Glendale
+glendale_stars = session.query(Actor) \
+    .join(ContactDetails) \
+    .filter(ContactDetails.address.ilike('%glendale%')) \
+    .all()
+
+print('### Dwayne Johnson movies:')
+for actor in glendale_stars:
+    print(f'{actor.name} has a house in Glendale')
+print('')
